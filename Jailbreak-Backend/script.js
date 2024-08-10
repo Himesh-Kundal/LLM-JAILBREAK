f021913355c19090385ab6788dc9220313750c05
Himesh-Kundal <himesh.kundal@kgpian.iitkgp.ac.in>
2024-08-11 03:48:42 +0530

    backend

--- Jailbreak-Backend/script.js
+++ Jailbreak-Backend/script.js
@@ -1,18 +1,44 @@
-const express = require('express');
+const Groq = require("groq-sdk");
+const express = require('express')
 const cors = require('cors');
-const z= require('zod');
+const dotenv = require('dotenv');
+dotenv.config();
+
 const app = express();
+const port = 3000
 
-const port = 3000;
-
+app.use(cors());
+const groq = new Groq({ apiKey: process.env.GROQ_API_KEY });
 
 app.use(express.json());
-app.use(cors());
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+app.post('/', (req, res) => {
+    console.log(req.body);
+    const message=(req.body.message);
+    main(message).then((response) => {
+        res.send(response);
+    });
+})
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
-});
+  console.log(`Example app listening on port ${port}`)
+})
+
+
+
+async function main(message) {
+    const chatCompletion = await getGroqChatCompletion(message);
+    return (chatCompletion.choices[0]?.message?.content || "");
+  }
+
+async function getGroqChatCompletion(message) {
+    return groq.chat.completions.create({
+        messages: [
+        {
+            role: "user",
+            content: `${message}`,
+        },
+        ],
+        model: "llama3-8b-8192",
+    });
+}
