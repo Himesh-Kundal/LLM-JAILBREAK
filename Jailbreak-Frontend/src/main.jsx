b772cf7b1aa8f5dd28baa2d6aa384dd44bed6342
Himesh-Kundal <himesh.kundal@kgpian.iitkgp.ac.in>
2024-08-11 06:20:07 +0530

    cleanup

--- Jailbreak-Frontend/src/main.jsx
+++ Jailbreak-Frontend/src/main.jsx
@@ -1,13 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App2 from './App2.jsx'
-// import './index.css'
+import App from './App.jsx'
 import './styles.css'
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    {/* <App /> */}
-    <App2 />
+    <App />
   </StrictMode>,
 )
